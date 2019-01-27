2847472d479d60731e6dfa86b909fc736f551d79
Jor Bashllari <altnate2000@gmail.com>
2019-01-28 00:41:45 +0100

    start headers

---
+++ include/shell.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_minishell_2018
+** File description:
+** something small
+*/
+
+#ifndef SHELL_H
+    #define SHELL_H
+
+    #include "my.h"
+    #include "typeshell.h"
+
+cmd_t *mkcmd(char *);
+void rmcmd(cmd_t *);
+
+#endif
---
+++ include/typeshell.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_minishell_2018
+** File description:
+** something small
+*/
+
+#ifndef TYPESHELL_H
+    #define TYPESHELL_H
+
+typedef struct cmd {
+    int ac;
+    char **av;
+} cmd_t;
+
+#endif
